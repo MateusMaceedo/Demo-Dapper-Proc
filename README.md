323f0475bb58a040076d953f522a6edf55dc042b
MateusMacedo <mateusouza2014@live.com>
2022-02-26 02:31:42 -0300

    feat: Estrutura inicial do projeto

---
+++ README.md
@@ -0,0 +1,16 @@
+### Steps:
+
+1. CRUD operations in asp.net 6 MVC using SQL Stored procedures and Dapper C#
+2. Complete use of C# Dapper in ASP.NET 6 MVC.
+3. Ajax call in ASP.Net 6
+4. Dependency injection in ASP .NET 6 MVC
+5. Registering services in the program.cs class
+
+#### Instal Packages Imnportant
+
+- Microsoft.EntityFrameworkCore
+- Microsoft.EntityFrameworkCore.Design
+- Microsoft.EntityFrameworkCore.SqlServer
+- Microsoft.VisualStudio.Web.CodeGeneration.Design
+- System.Data.SqlClient
+- Dapper
---
+++ StudentRecordManagementApp/Services/StudentServices.cs
@@ -0,0 +1,67 @@
+﻿using Dapper;
+using StudentRecordManagementApp.Models;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace StudentRecordManagementApp.Services
+{
+    public class StudentServices : IStudentService
+    {
+        private readonly IConfiguration _configuration;
+
+        public StudentServices(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            ConnectionString = _configuration.GetConnectionString("DbConnection");
+            ProviderName = "System.Data.SqlClient"; 
+        }
+
+        public string? ConnectionString { get; set; }
+        public string? ProviderName { get; set; }
+
+        public IDbConnection Connection
+        {
+            get { return new SqlConnection(ConnectionString); }
+        }
+
+        public string InsertStudent(Students model)
+        {
+            using (IDbConnection dbConnection = Connection)
+            {
+                dbConnection.Open();
+                var student = dbConnection.Query<Students>("InsertStudentRecord", model, commandType: CommandType.StoredProcedure).ToList();
+                dbConnection.Close();
+            }
+
+            return "";
+        }
+
+        public List<Students> GetStudentsList()
+        {
+            List<Students> result = new();
+            try
+            {
+                using (IDbConnection dbConnection = Connection)
+                {
+                    dbConnection.Open();
+                    result = dbConnection.Query<Students>("InsertStudentList", commandType: CommandType.StoredProcedure).ToList();
+
+                    dbConnection.Close();
+
+                    return result;
+                }
+            }
+            catch (Exception ex)
+            {
+                _ = ex.Message;
+                return result;
+            }
+        }
+    }
+
+    public interface IStudentService
+    {
+        public string InsertStudent(Students model);
+        public List<Students> GetStudentsList();
+    }
+}
